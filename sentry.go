553db15749575866755f6d51ca5545917d9f4cbc
Greg Weber <gweber@digitalmint.io>
2023-09-17 07:10:26 -0500

    log request when JSON parse fails

--- sentry.go
+++ sentry.go
@@ -6,8 +6,8 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-  "strings"
-  "regexp"
+	"regexp"
+	"strings"
 
 	"github.com/getsentry/sentry-go"
 	"go.uber.org/zap"
@@ -92,7 +92,7 @@ func (lsf LogSentrySendFailures) RoundTrip(req *http.Request) (*http.Response, e
 		} else {
 			event := sentry.Event{}
 			if err := json.Unmarshal(body, &event); err != nil {
-				lsf.Logger.Errorw("Sentry event send failure: error recovering request json", "status", statusCode, "error", err)
+				lsf.Logger.Errorw("Sentry event send failure: error recovering request json", "status", statusCode, "error", err, "request", string(body))
 			}
 			var rspBodyStr string
 			if resp != nil {
@@ -117,31 +117,31 @@ func (lsf LogSentrySendFailures) RoundTrip(req *http.Request) (*http.Response, e
 // NormalizeUrlPathForSentry takes a url path string and replaces any path part that contains a number with a standard placeholder value.
 // This allows for better error grouping at Sentry for urls that may contain dynamic values (UUID for example) but are basically the same URL in general
 func NormalizeUrlPathForSentry(_url string, placeholder string) (string, error) {
-        if placeholder == "" {
-                placeholder = "-omitted-"
-        }
-        newPath := _url
-        u, err := url.Parse(_url)
-        if err != nil {
-                return newPath, err
-        } else { // Split the URL path into individual parts
-                pathParts := strings.Split(u.Path, "/")
+	if placeholder == "" {
+		placeholder = "-omitted-"
+	}
+	newPath := _url
+	u, err := url.Parse(_url)
+	if err != nil {
+		return newPath, err
+	} else { // Split the URL path into individual parts
+		pathParts := strings.Split(u.Path, "/")
 
-                // Regular expression to match numeric parts of the path
-                numericRegex := regexp.MustCompile("[0-9]+")
+		// Regular expression to match numeric parts of the path
+		numericRegex := regexp.MustCompile("[0-9]+")
 
-                // Iterate over each part of the path
-                for i, part := range pathParts {
-                        // Check if the part contains a number
-                        if part != "v1" && part != "v2" && numericRegex.MatchString(part) {
-                                // Replace the numeric part with "placeholder"
-                                pathParts[i] = placeholder
-                        }
-                }
+		// Iterate over each part of the path
+		for i, part := range pathParts {
+			// Check if the part contains a number
+			if part != "v1" && part != "v2" && numericRegex.MatchString(part) {
+				// Replace the numeric part with "placeholder"
+				pathParts[i] = placeholder
+			}
+		}
 
-                // Join the path parts back into a single string with "/"
-                newPath = strings.Join(pathParts, "/")
-                newPath = strings.TrimSuffix(newPath, "/")
-        }
-        return newPath, nil
+		// Join the path parts back into a single string with "/"
+		newPath = strings.Join(pathParts, "/")
+		newPath = strings.TrimSuffix(newPath, "/")
+	}
+	return newPath, nil
 }
